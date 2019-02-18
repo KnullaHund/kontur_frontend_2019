4aa0fdd27d1adff76c03a4aeb0c4b97b499a2d79
KnullaHund <kozyr96824@gmail.com>
2019-02-18 13:23:09 +0700

    Отдебажил

--- ParsTODO.js
+++ ParsTODO.js
@@ -1,25 +1,26 @@
 const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
 
-function CreateTodoList() {
-  let filePaths = getAllFilePathsWithExtension('./' ,'js'),
+function createTodoList() {
+  let filePaths = [],
       fileContent,
       todoList = [],
       todoPoints;
+  filePaths = getAllFilePathsWithExtension('./' ,'js')
   for (let i = 0; i < filePaths.length; i++){
     fileContent = readFile(filePaths[i]);
     todoPoints = GetNecessaryPoints(fileContent);
     todoList = todoList.concat(todoPoints);
   }
-  return todoPoints;
+  return filePaths.todoList;
 }
 
 function GetNecessaryPoints(fileContent) {
-  const todostart = /\/\/\s*TODO\s*:*/i;
+  const todoStart = /\/\/\s*TODO\s*:*/i;
   let nextStringPosition = 0,
       todoPoint,
       todoPoints = [];
-  while (fileContent.indexOf(todostart, nextStringPosition) !=-1) {
-    let todoStartPosition = fileContent.indexOf(todostart, nextStringPosition);
+  while (fileContent.regExpIndexOf(todoStart, nextStringPosition) !=-1) {
+    let todoStartPosition = fileContent.regExpIndexOf(todoStart, nextStringPosition);
     if (-1 != fileContent.indexOf('\n', todoStartPosition)) {
       nextStringPosition = fileContent.indexOf('\n', todoStartPosition);
       todoPoint = CreateTodoPoint(fileContent, nextStringPosition, todoStartPosition);
@@ -36,8 +37,13 @@ function GetNecessaryPoints(fileContent) {
 }
 
 function CreateTodoPoint(fileContent, nextStringPosition, todoStartPosition) {
-  todoPoint = fileContent.slice(todoStartPosition, nextStringPosition);
+  let todoPoint = fileContent.substring(todoStartPosition, nextStringPosition);
   return todoPoint;
 }
 
-console.log(CreateTodoList());
+String.prototype.regExpIndexOf = function(regExp, startPosition) {
+  let position = this.substring(startPosition || 0).search(regExp);
+  return (position != -1) ? (position + (startPosition || 0)) : position;
+}
+
+console.log(createTodoList());
